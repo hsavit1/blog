ee87c37c28f2250abf674c902c29c65a00206697
Henry <henrysavit@gmail.com>
2018-10-20 12:02:18 -0400

    fixed about

--- pages/about.js
+++ pages/about.js
@@ -1,113 +1,114 @@
-import Page from '../layouts/main';
-import Head from 'next/head';
-import P from '../components/post/paragraph';
-import TITLE from '../components/about/title';
-import SUBTITLE from '../components/about/subtitle';
+import Page from "../layouts/main";
+import Head from "next/head";
+import P from "../components/post/paragraph";
+import TITLE from "../components/about/title";
+import SUBTITLE from "../components/about/subtitle";
+import Link from "next/link";
 
 export default () => (
-	<Page>
-		<Head>
-			<title>About</title>
-		</Head>
-		<div className="about">
-			<TITLE>About</TITLE>
+  <Page>
+    <Head>
+      <title>About</title>
+    </Head>
+    <div className="about">
+      <TITLE>About</TITLE>
 
-			<SUBTITLE>
-				I will make you a less shitty human, if you let me.
-			</SUBTITLE>
+      <SUBTITLE>I will make your existence better, if you let me.</SUBTITLE>
 
-			<P>
-				I made this site to open up my mind to you, the humble and
-				inquisitive reader. You'll be glad that you came to visit.
-			</P>
+      <P>
+        This site is my way of opening my mind to you, dear reader. By doing so
+        you will become inspired and you will leave better clarity about what it
+        means to live on this floating rock than you've had before. This isn't
+        your typical thought dump shit blog or lame professional site. You'll
+        actually be glad that you came to visit.
+      </P>
 
-			<P>
-				For your reading pleasure, you'll find some longform musings
-				related to the evolution of cryptocurrencies, the purpose of the
-				internet and other design meditations, some technical stuff, and
-				some satire.
-			</P>
+      <P>
+        For your pleasure, you'll find my take on the purpose of the
+        internet, how humanity can strategize against the very real threat of climate
+        change, some longform musings related to the evolution of
+        cryptocurrencies, some technical stuff, and some satire.
+      </P>
 
-			<P>
-				You might as well check out some of my essays. Maybe you’ll find
-				something useful about our complicated relationship with
-				technology, or about the some existential crises that we face in
-				the anthropocene. Maybe you'll even learn something. Or maybe
-				it'll take you to a dark, dark, dark place and you'll find
-				yourself in a basement huddled into a ball overwhelmed with
-				fear. You'll just have to read to find out.
-			</P>
+      <P>
+        You might as well check out some of my{" "}
+        <Link prefetch href="/longform">
+          <a className="side">Essays</a>
+        </Link>
+        . Maybe you’ll find something useful about our complicated relationship
+        with technology, or about the some existential crises that we face in
+        the age of the anthropocene. Maybe you'll even learn something. Or maybe
+        it'll take you to a dark, dark, dark place and you'll find yourself in a
+        basement huddled into a ball overwhelmed with fear. If you read what I
+        have, I will change the way you see.
+      </P>
 
-			<P>
-				The crypto market right now is a hellscape of factoids and I
-				feel that it is necessary to attempt to break it down - that
-				could be by assessing whitepapers, consensus mechanisms, or even
-				the big ideas and visions - if we're going to make any sense of
-				this{' '}
-				<a
-					target="_blank"
-					href="https://marymackey.com/wp-content/uploads/2014/11/Pandemonium.jpg"
-				>
-					pandemonium
-				</a>{' '}
-				and emerge with something of real value. That's why I dedicated
-				a whole section of my site to it.
-			</P>
+      <P>
+        Concerning cryptocurrencies - the cryptocurrency sphere right now offers
+        so much by way of innovation yet is still a hellscape of factoids
+        propagated by charlatans. I feel that it's necessary to attempt to
+        analyze it - that could be by assessing whitepapers, consensus
+        mechanisms, or even the big ideas and visions - if we're going to emerge
+        with a technology that is of real value to humanity. Otherwise, most of
+        the time and effort spent by developers trying to innovate with
+        cryptocurrencies will go for naught, and a whole bunch of valuable
+        manhours that could have been spent on other means will That's why I
+        dedicated a whole section of my site to it.
+      </P>
 
-			<P>
-				Zooming out of the world of crypto politics is the world of real
-				politics, where things have just turned so insane that
-				satire has become a coping mechanism for me.
-			</P>
+      {/* <P>
+        Zooming out of the world of crypto politics is the world of real
+        politics, where things have just turned so insane that satire has become
+        a coping mechanism for me.
+      </P> */}
 
-			<P>
-				Lastly, there's a bookshelf section where you can find some
-				reflections on things that I've read. Books are entry points for
-				pitches of real ideas. Discussion needs to continue outside of
-				the text if these ideas are to have any legs. I don't get to
-				review all of the books that I read, but I try to elucidate on
-				the ones that I find the most thought provoking.
-			</P>
+      <P>
+        There's also a bookshelf section where you can find some reflections on
+        things that I've read. Books are entry points for pitches of real ideas.
+        Discussion needs to continue outside of the text if these ideas are to
+        have any legs. I don't get to analyze all of the books that I read, but
+        I try to elucidate on the ones that I find the most galvanizing.
+      </P>
 
-			<P>
-				You can find the source for this page on{' '}
-				<a target="_blank" href="https://github.com/hsavit1/blog">
-					Github
-				</a>
-				. It's built with React and Next.js and hosted with Zeit. The
-				design is minimalist for a reason - I want you to focus on the
-				content. As a bonus, the pages render pretty fast.
-			</P>
+      <P>
+        You can find the source for this page on{" "}
+        <a target="_blank" href="https://github.com/hsavit1/blog">
+          Github
+        </a>
+        . It's built with React and Next.js and hosted with Zeit. The design is
+        minimalist for a reason - I want you to focus on the content. As a
+        bonus, the pages render pretty fast.
+      </P>
 
-			<P>
-				If you like what you see, for now, you can message me on{' '}
-				<a target="_blank" href="https://twitter.com/cantbemorewrong">
-					Twitter
-				</a>
-				. And if you really like what you see and want to support my
-				writing, do the friendly thing and send me a donation through{' '}
-				<a
-					target="_blank"
-					href="https://brave.com/faq-payments/#what-is-brave-payments"
-				>
-					Brave payments
-				</a>
-				.
-			</P>
-		</div>
+      <P>
+        If you like what you see, for now, you can message me on{" "}
+        <a target="_blank" href="https://twitter.com/cantbemorewrong">
+          Twitter
+        </a>
+        . And if you really like what you see and want to support my writing, do
+        the friendly thing and send me a donation through{" "}
+        <a
+          target="_blank"
+          href="https://brave.com/faq-payments/#what-is-brave-payments"
+        >
+          Brave payments
+        </a>
+        .
+      </P>
+    </div>
 
-		<style jsx>{`
-			.about {
-				max-width: 650px;
-				margin: auto;
-				font-size: 14px;
-			}
-		`}</style>
-		<style jsx global>{`
-			body {
-				width: 100%;
-				overflow-x: hidden;
-			}
-		`}</style>
-	</Page>
+    <style jsx>{`
+      .about {
+        max-width: 650px;
+        margin: auto;
+        font-size: 14px;
+      }
+    `}</style>
+    <style jsx global>{`
+      body {
+        width: 100%;
+        overflow-x: hidden;
+      }
+    `}</style>
+  </Page>
 );
